402c0e213d3af053b8b1ca6dd9d229bf9a1c3dbc
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-08-20 10:23:22 -0500

    Se agrega msvc-security con el fin de superar el reto 2 de JWT y demas

---
+++ msvc-security/src/test/java/com/taller/msvc_security/MsvcSecurityApplicationTests.java
@@ -0,0 +1,15 @@
+package com.taller.msvc_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSecurityApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+		//No problems yet
+	}
+
+}
