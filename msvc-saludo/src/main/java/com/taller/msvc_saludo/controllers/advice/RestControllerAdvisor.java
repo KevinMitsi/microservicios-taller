78ee02dd0110b406695f3780b1bf78a6f53bd236
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-08-20 10:14:34 -0500

    Punto 1 completado en su totalidad

--- msvc-saludo/src/main/java/com/taller/msvc_saludo/controllers/advice/RestControllerAdvisor.java
+++ msvc-saludo/src/main/java/com/taller/msvc_saludo/controllers/advice/RestControllerAdvisor.java
@@ -1,4 +1,18 @@
 package com.taller.msvc_saludo.controllers.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+public class RestControllerAdvisor {
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleMissingParameter(MissingServletRequestParameterException ex) {
+        String paramName = ex.getParameterName();
+        String message = "El parámetro '" + paramName + "' es obligatorio";
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+}
