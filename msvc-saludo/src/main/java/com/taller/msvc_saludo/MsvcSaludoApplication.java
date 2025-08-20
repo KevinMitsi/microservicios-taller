f06821ad250fa52b512c0dac93f2313990bbf1ea
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-08-20 09:39:00 -0500

    Modularizacion del proyecto

---
+++ msvc-saludo/src/main/java/com/taller/msvc_saludo/MsvcSaludoApplication.java
@@ -0,0 +1,13 @@
+package com.taller.msvc_saludo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcSaludoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcSaludoApplication.class, args);
+	}
+
+}
---
+++ msvc-saludo/src/test/java/com/taller/msvc_saludo/MsvcSaludoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.msvc_saludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSaludoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
