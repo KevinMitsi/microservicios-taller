0c468c28a58b499254dfde447de2f985e20d7648
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-08-20 09:53:20 -0500

    Reto 1 completado

---
+++ msvc-saludo/src/main/java/com/taller/msvc_saludo/controllers/GreetingsController.java
@@ -0,0 +1,17 @@
+package com.taller.msvc_saludo.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/saludo")
+    public ResponseEntity<String> greet(@RequestParam String nombre) {
+        return ResponseEntity.ok("Hola " + nombre + ", bienvenido al microservicio de saludo!");
+    }
+
+}
