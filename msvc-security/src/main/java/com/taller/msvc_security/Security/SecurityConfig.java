44fbb2b83997f59c4d29d5d580b76c7fccd236ed
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2025-09-02 21:19:01 -0500

    Se permiten las rutas de OpenAPI para ingresar al UI de Swagger

--- msvc-security/src/main/java/com/taller/msvc_security/Security/SecurityConfig.java
+++ msvc-security/src/main/java/com/taller/msvc_security/Security/SecurityConfig.java
@@ -33,6 +33,16 @@ public class SecurityConfig {
                         // Rutas públicas
                         .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/newUser").permitAll()
+                        // Swagger (importante permitir todos los recursos)
+                        // Swagger + OpenAPI
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
                         .requestMatchers("/api/auth/password-recovery").permitAll()
                         .requestMatchers("/api/auth/password-reset").permitAll()
                         // Todas las demás rutas requieren autenticación
