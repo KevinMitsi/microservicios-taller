c7ab6a0e82c53f4028580a332f74abd9f3239170
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-09-08 07:55:54 -0500

    Agregado msvc-delivery para el envio de mesajes por correo electronico y/o SMS de forma asincrona

---
+++ msvc-delivery/src/main/java/com/taller/msvc_delivery/MsvcDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.taller.msvc_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcDeliveryApplication.class, args);
+	}
+
+}
---
+++ msvc-delivery/src/test/java/com/taller/msvc_delivery/MsvcDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.msvc_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
