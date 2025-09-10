20f25acf7b48aea015e8e3317cfd894205a32bce
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-09-09 22:01:34 -0500

    Se agregan anotaciones dentro de las impls para que si se puedan comunicar con rbtmq

--- msvc-delivery/src/main/java/com/taller/msvc_delivery/services/impl/EmailConsumerImpl.java
+++ msvc-delivery/src/main/java/com/taller/msvc_delivery/services/impl/EmailConsumerImpl.java
@@ -2,9 +2,12 @@ package com.taller.msvc_delivery.services.impl;
 
 import com.taller.msvc_delivery.DTO.NotificationDTO;
 import com.taller.msvc_delivery.services.EmailConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 public class EmailConsumerImpl implements EmailConsumer {
+
     @Override
+    @RabbitListener(queues = "queue.email")
     public void processEmail(NotificationDTO notification) {
 
     }
--- msvc-delivery/src/main/java/com/taller/msvc_delivery/services/impl/SmsConsumerImpl.java
+++ msvc-delivery/src/main/java/com/taller/msvc_delivery/services/impl/SmsConsumerImpl.java
@@ -2,9 +2,11 @@ package com.taller.msvc_delivery.services.impl;
 
 import com.taller.msvc_delivery.DTO.NotificationDTO;
 import com.taller.msvc_delivery.services.SmsConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 public class SmsConsumerImpl implements SmsConsumer {
     @Override
+    @RabbitListener(queues = "queue.sms")
     public void processSms(NotificationDTO notification) {
 
     }
