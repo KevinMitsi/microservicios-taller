bedae70faf3aa0a93bbb32ffee9f697eb980b731
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2025-09-11 11:27:30 -0500

    Se corrigen las validaciones manuales a validaciones con Jakarta dentro de los DTOs.

--- msvc-security/src/main/java/com/taller/msvc_security/Models/UserRegistrationRequest.java
+++ msvc-security/src/main/java/com/taller/msvc_security/Models/UserRegistrationRequest.java
@@ -1,5 +1,9 @@
 package com.taller.msvc_security.Models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +12,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegistrationRequest {
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    @Size(min = 3, message = "El nombre de usuario debe tener al menos 3 caracteres")
     private String username;
+
+    @NotBlank(message = "El correo electrónico es obligatorio")
+    @Email(message = "El correo electrónico debe ser válido")
     private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
     private String password;
+
     private String firstName;
     private String lastName;
     private String mobileNumber;
-
 
 }
